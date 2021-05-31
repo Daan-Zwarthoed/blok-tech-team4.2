702c198a29a44276130d92653c1423a3d7d41d97
MilanSteman <milansteman@gmail.com>
2021-05-31 17:21:52 +0200

    Added: Database Connection

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
+const mongoose = require("mongoose");
+const router = require("src/routes/router.js");
 
 const app = express();
 const port = process.env.PORT || 3000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
 // Static files
 app.use(express.static("static/public"));
@@ -19,3 +22,12 @@ nunjucks.configure("src/views", {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+
+mongoose
+    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connected to Database");
+    })
+    .catch((err) => {
+        throw err;
+    });

bbd6bdda9b6a983a59156b3b39dee296474e1c1e
Daan Zwarthoed <78368488+DaanZwarthoed@users.noreply.github.com>
2021-05-31 13:59:52 +0200

    basic routing and header and footer

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,1 +1,13 @@
 // Router
+
+module.exports = (function () {
+  "use strict";
+
+  var routes = require("express").Router();
+
+  routes.get("/", (req, res) => {
+    res.render("index.njk");
+  });
+
+  return routes;
+})();

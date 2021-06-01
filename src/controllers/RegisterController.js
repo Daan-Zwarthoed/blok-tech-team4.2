827929630ef5651e40fb9c08c29718a4049f7973
Daan-Zwarthoed <74141268+Daan-Zwarthoed@users.noreply.github.com>
2021-06-01 10:35:39 +0200

    Update RegisterController.js

--- src/controllers/RegisterController.js
+++ src/controllers/RegisterController.js
@@ -1,12 +1,10 @@
 const User = require('../models/User');
 
 const getRegister = (req, res) => {
-    console.log('test')
     res.render("register.njk");
 }
 
 const registerUser = (req, res) => {
-    console.log(req.body);
     res.render("register.njk")
 }
 

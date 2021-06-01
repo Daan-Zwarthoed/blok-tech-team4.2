b5b5bb9974a43aa64857777e7013ff804a7148ba
Daan Zwarthoed <78368488+DaanZwarthoed@users.noreply.github.com>
2021-06-01 14:47:29 +0200

    added the basis for the chat function

--- server.js
+++ server.js
@@ -11,18 +11,40 @@ if (process.env.DB_USER) {
   uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 }
 
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+let sortAlphabets = function (text) {
+  return text.split("").sort().join("");
+};
+
 app.use(express.static("static/public"));
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use("/", router);
 
+io.on("connection", (socket) => {
+  socket.on("join room", (message) => {
+    socket.join(sortAlphabets(`${message.userSelf}${message.userOther}`));
+  });
+
+  socket.on("chat message", (message) => {
+    io.to(sortAlphabets(`${message.userSelf}${message.userOther}`)).emit(
+      "chat message",
+      { message: message.message, userSelf: message.userSelf }
+    );
+  });
+});
+
 nunjucks.configure("src/views/", {
   autoescape: true,
   express: app,
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 

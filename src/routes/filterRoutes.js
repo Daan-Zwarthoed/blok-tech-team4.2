c5e9f1cf4e59ce7d5bb8bfd2f9e31fd5b2b307a6
MaylinnK <maylinn.kwakman@hva.nl>
2021-06-08 14:33:57 +0200

    Finished filter feature (results page still bugged)

--- src/routes/filterRoutes.js
+++ src/routes/filterRoutes.js
@@ -1,61 +1,19 @@
 const router = require("express").Router();
-const gameList = ["CSGO", "CoD", "Tetris", "WoW"]
+const gameList = ["CSGO", "CoD", "Tetris", "WoW"];
+const User = require("../models/User");
 const FilterController = require("../controllers/FilterController");
-const userList = [{
-        "name": "Beasthunter69",
-        "topGame": "CSGO",
-        "playstyle": "Competitive",
-        "time": "5+ hours per day"
-    },
-    {
-        "name": "xXKirito420Xx",
-        "topGame": "CSGO",
-        "playstyle": "Competitive",
-        "time": "-5 hours per day"
-    },
-    {
-        "name": "rinrin",
-        "topGame": "Tetris",
-        "playstyle": "Casual",
-        "time": "A few times per week"
-    },
-    {
-        "name": "xnamewastakenx",
-        "topGame": "WoW",
-        "playstyle": "Casual",
-        "time": "Once per week"
-    }
-];
 
 router.get("/", (req, res) => {
-    res.render("pages/filter/chooseGame.njk", { gameList });
+    res.render("pages/filter/filter.njk", { title: "Filter", gameList, User });
 });
 
-router.get("/CSGO", (req, res) => {
-    res.render("pages/filter/filter.njk", { title: "CSGO", gameList, userList });
-});
-
-router.post("/CSGO", (req, res) => {
+router.post("/", async(req, res) => {
     const form = req.body;
     console.log(form);
-    const options = { sort: { time: -1 } };
     const query = form;
-    const filteredUsers = userList.find(query, options).toArray();
+    const filteredUsers = await User.find(query);
     console.log(filteredUsers);
     res.render("pages/filter/matches.njk", { title: "Matches", filteredUsers });
-});
-
-
-router.get("/CoD", (req, res) => {
-    res.render("pages/filter/filter.njk", { title: "CoD", gameList });
-});
-
-router.get("/Tetris", (req, res) => {
-    res.render("pages/filter/filter.njk", { title: "Tetris", gameList });
-});
-
-router.get("/WoW", (req, res) => {
-    res.render("pages/filter/filter.njk", { title: "WoW", gameList });
 });
 
 module.exports = router;

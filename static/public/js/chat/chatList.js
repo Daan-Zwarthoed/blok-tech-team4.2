cf6961fa98e84593653631dc83cb95cb7964f52d
Daan Zwarthoed <78368488+DaanZwarthoed@users.noreply.github.com>
2021-06-09 16:00:25 +0200

    added some styling to chatlist

--- static/public/js/chat/chatList.js
+++ static/public/js/chat/chatList.js
@@ -4,7 +4,7 @@ const hiddenImage = document.querySelector('.hiddenImage');
 const loadImage = function (image) {
     hiddenImage.src = image.src;
     if (hiddenImage.width === 0) {
-        image.src = '/images/defaultUser.png';
+        image.src = '/images/defaultUser.jpeg';
     }
     hiddenImage.src = '';
 };

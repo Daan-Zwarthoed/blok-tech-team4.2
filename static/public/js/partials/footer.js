bc656c3c45e2f1feb92495cc4e0c4426b1cf1ac3
MilanSteman <43573831+MilanSteman@users.noreply.github.com>
2021-06-09 20:55:35 +0200

    Update footer.js
    Made a switch statement instead of multiple if statements.

--- static/public/js/partials/footer.js
+++ static/public/js/partials/footer.js
@@ -1,8 +1,14 @@
 const currentPage = window.location.pathname.split('/')[1];
 const footerList = document.querySelector('.bottom-nav > nav > ul');
 
-if (currentPage === 'filter') footerList.children[0].classList.add('currentPage');
-
-if (currentPage === 'chat') footerList.children[1].classList.add('currentPage');
-
-if (currentPage === 'profiles') footerList.children[2].classList.add('currentPage');
+switch (currentPage) {
+   case 'filter':
+    footerList.children[0].classList.add('currentPage');
+    break;
+  case 'chat':
+    footerList.children[1].classList.add('currentPage');
+    break;
+  case 'profiles':
+    footerList.children[2].classList.add('currentPage');
+    break;
+}

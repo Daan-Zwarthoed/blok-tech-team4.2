48f89b4731b6cace42e6298b595839598cc62880
MilanSteman <milansteman@gmail.com>
2021-06-10 02:06:21 +0200

    Added: error states to registering

--- static/public/js/register/scroll.js
+++ static/public/js/register/scroll.js
@@ -1,5 +1,6 @@
 const formElem = document.querySelector('form');
 const formLinks = document.querySelectorAll('.next-step');
+const formInput = document.querySelectorAll('form input');
 const generalInformationInput = document.querySelectorAll('#general-information input');
 const visualInformation = document.querySelector('#visual-information');
 const visualInformationInput = document.querySelectorAll('#visual-information input');
@@ -13,24 +14,32 @@ const onboardInformation = document.querySelector('#onboard-information');
  * @param {*} form = the form which is being used
  * @param {*} nextInfo = the next sequence of information
  */
-const checkInput = (arr, form, nextInfo) => {
+const checkInput = (arr, form, nextInfo, event) => {
     const noInput = [];
 
     // Loop through every input to find the fields that haven't been filled in yet
     arr.forEach((input) => {
         if (input.value.length === 0) {
             // Push the input to the array which contains every input field that has no input yet
-            noInput.push(this.input);
-        }
-
-        // If there are no input fields, go to the next sequence
-        if (noInput.length === 0) {
-            form.scroll({
-                left: nextInfo.offsetLeft,
-                behavior: 'smooth',
-            });
+            noInput.push(input);
+            input.classList.add('warning');
         }
     });
+
+    // If there are no input fields, go to the next sequence
+    if (noInput.length === 0) {
+        form.scroll({
+            left: nextInfo.offsetLeft,
+            behavior: 'smooth',
+        });
+    } else {
+        const button = event.target;
+        button.classList.add('warning');
+
+        setTimeout(() => {
+            button.classList.remove('warning');
+        }, 3000);
+    }
 };
 
 /**
@@ -41,18 +50,30 @@ const checkInput = (arr, form, nextInfo) => {
 const scrollElement = (event) => {
     switch (event.target) {
         case formLinks[0]:
-            // Prevent default behavior for smooth interactions when Javascript is enabled.
+            // Prevent default behavior for smooth interactions when Javascript is enabled
             event.preventDefault();
-            checkInput(generalInformationInput, formElem, visualInformation);
+            checkInput(generalInformationInput, formElem, visualInformation, event);
             break;
         case formLinks[1]:
-            // Prevent default behavior for smooth interactions when Javascript is enabled.
+            // Prevent default behavior for smooth interactions when Javascript is enabled
             event.preventDefault();
-            checkInput(visualInformationInput, formElem, onboardInformation);
+            checkInput(visualInformationInput, formElem, onboardInformation, event);
             break;
     }
 };
 
+/**
+ * EventListener for the next step buttons.
+ */
 formLinks.forEach((formLink) => {
     formLink.addEventListener('click', scrollElement);
+});
+
+/**
+ * This function removes the error state on any input when the user clicks on this input.
+ */
+formInput.forEach((input) => {
+    input.addEventListener('click', () => {
+        input.classList.remove('warning');
+    });
 });

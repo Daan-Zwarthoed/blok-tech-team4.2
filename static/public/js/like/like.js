66b9154f9d54e149870efb0e331cacbe71a3f866
akshay197 <akshay.ak@hotmail.com>
2021-06-14 15:00:47 +0200

    Swiping feature added

--- static/public/js/like/like.js
+++ static/public/js/like/like.js
@@ -1,3 +1,7 @@
+// https://www.npmjs.com/package/swipe-listener
+// https://browserify.org/
+
+const SwipeListener = require('swipe-listener');
 const dislike = document.querySelector('#dislike');
 const like = document.querySelector('#like');
 const submit = document.querySelector('.verzend');
@@ -8,6 +12,19 @@ submit.hidden = true;
 /**
  * Functie
  */
+
+card.addEventListener('swipe', (event) => {
+    const directions = event.detail.directions;
+    switch (directions) {
+        case directions.left:
+            dislike.checked = true;
+            formulier.submit();
+        case directions.right:
+            like.checked = true;
+            formulier.submit();
+    }
+});
+
 like.addEventListener('click', function () {
     formulier.submit();
     console.log('test');

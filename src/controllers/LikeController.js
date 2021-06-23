b80c739b7be38b7abc64bd89e21716c180bb2637
MaylinnK <maylinn.kwakman@hva.nl>
2021-06-23 16:01:50 +0200

    Added proper error handling
    Removed client-side filter validation;
    Added express-validator package;
    Added validation checks to likeRoutes.js;
    Removed 'required' on form inputs, so the form can be submitted and the validation checks can be made.
    Added error handling to likeController.js

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -2,6 +2,7 @@
  * Match Controller
  */
 
+const { validationResult } = require('express-validator');
 const Game = require('../models/Game');
 const User = require('../models/User');
 
@@ -11,50 +12,64 @@ let chosenGame;
 
 // functie getSimilarUsers, deze functie zoekt naar gebruikers die dezelfde spelletjes als jou hebben geliked
 const getSimilarUsers = (req, res) => {
-    const myUser = req.user; // dit is de ingelogd gebruiker
-    Game.find().then((games) => {
-        if (req.body.games) {
-            chosenGame = req.body.games; // Gekozen spel om op te filteren
-        }
-        let filteredUsers;
-        // Haalt gebruikersnamen op van gebruikers die het gekozen spel geliked hebben
-        if (games.filter((game) => game.titleSlug == chosenGame).length > 0) {
-            filteredUsers = games.filter((game) => game.titleSlug == chosenGame)[0].likedBy;
-        }
-
-        User.find().then((userList) => {
-            const profiles = [];
-            // Filtert de gebruikerscollectie op de gebruikersnamen in filteredUsers en pushed the matchende profielen naar profiles
-            userList.forEach((user) => {
-                if (filteredUsers) {
-                    if (filteredUsers.includes(user.username)) {
-                        profiles.push(user);
-                    }
-                }
-            });
-            // Filtert profielen van gebruikers op playtime, playstyle, likes, dislikes en verwijdert eigen naam van de lijst.
-            const filteredProfiles = [...new Set(profiles)].filter(
-                (profile) =>
-                    profile.playstyle === req.body.playstyle &&
-                    profile.playtime === req.body.playtime &&
-                    profile.username !== myUser.username &&
-                    !myUser.likedBy.includes(profile.username) &&
-                    !myUser.dislikedBy.includes(profile.username)
-            );
-
-            // Kiest 1 random gebruiker uit de lijst
-            displayedUser = filteredProfiles[Math.floor(Math.random() * filteredProfiles.length)];
-
-            if (filteredProfiles.length > 0) {
-                filteredProfiles.shift();
-            }
-            res.render('pages/like/like.njk', {
-                user: myUser,
-                displayedUser,
-                chosenGame,
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        User.findById(req.user._id, (err, user) => {
+            if (err) throw err;
+            const alert = errors.array();
+            res.render('pages/filter/filter.njk', {
+                title: 'Filter',
+                chosenGame: 'fortnite',
+                user,
+                alert,
             });
         });
-    });
+    } else {
+        const myUser = req.user; // dit is de ingelogde gebruiker
+        Game.find().then((games) => {
+            if (req.body.games) {
+                chosenGame = req.body.games; // Gekozen spel om op te filteren
+            }
+            let filteredUsers;
+            // Haalt gebruikersnamen op van gebruikers die het gekozen spel geliked hebben
+            if (games.filter((game) => game.titleSlug == chosenGame).length > 0) {
+                filteredUsers = games.filter((game) => game.titleSlug == chosenGame)[0].likedBy;
+            }
+
+            User.find().then((userList) => {
+                const profiles = [];
+                // Filtert de gebruikerscollectie op de gebruikersnamen in filteredUsers en pushed the matchende profielen naar profiles
+                userList.forEach((user) => {
+                    if (filteredUsers) {
+                        if (filteredUsers.includes(user.username)) {
+                            profiles.push(user);
+                        }
+                    }
+                });
+                // Filtert profielen van gebruikers op playtime, playstyle, likes, dislikes en verwijdert eigen naam van de lijst.
+                const filteredProfiles = [...new Set(profiles)].filter(
+                    (profile) =>
+                        profile.playstyle === req.body.playstyle &&
+                        profile.playtime === req.body.playtime &&
+                        profile.username !== myUser.username &&
+                        !myUser.likedBy.includes(profile.username) &&
+                        !myUser.dislikedBy.includes(profile.username)
+                );
+
+                // Kiest 1 random gebruiker uit de lijst
+                displayedUser = filteredProfiles[Math.floor(Math.random() * filteredProfiles.length)];
+
+                if (filteredProfiles.length > 0) {
+                    filteredProfiles.shift();
+                }
+                res.render('pages/like/like.njk', {
+                    user: myUser,
+                    displayedUser,
+                    chosenGame,
+                });
+            });
+        });
+    }
 };
 
 const userPreference = (req, res) => {
